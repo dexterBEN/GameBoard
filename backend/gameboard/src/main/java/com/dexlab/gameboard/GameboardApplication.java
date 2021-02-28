02069c1ebe5849b80c3729783ac77548ea2d9290
dexterBEN <bdextereric@yahoo.fr>
2021-02-28 15:06:59 +0100

    docs: enableh swagger

--- backend/gameboard/src/main/java/com/dexlab/gameboard/GameboardApplication.java
+++ backend/gameboard/src/main/java/com/dexlab/gameboard/GameboardApplication.java
@@ -11,8 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class GameboardApplication {
 
 	static String FB_BASE_URL="https://gameboard-b011e.firebaseio.com"; 
